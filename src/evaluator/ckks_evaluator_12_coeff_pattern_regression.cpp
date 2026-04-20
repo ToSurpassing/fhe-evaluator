91c56a56f3e85df9796786378daa74355529a98f
ToSurpassing <2211321@mail.nankai.edu.cn>
2026-04-20 11:23:07 +0800

    test: cover linear plus c5 tail plan

--- src/evaluator/ckks_evaluator_12_coeff_pattern_regression.cpp
+++ src/evaluator/ckks_evaluator_12_coeff_pattern_regression.cpp
@@ -57,6 +57,8 @@ std::vector<CaseDef> BuildCases() {
                 {ExpectedBlock{"linear", 1, "One"}}, 1, 0, 0, 0, 0, 0, 0},
         CaseDef{"only_c5", {0.0, 0.0, 0.0, 0.0, 0.0, 0.2}, "compact-active",
                 {}, 1, 3, 3, 3, 3, 3, 3},
+        CaseDef{"linear_plus_c5", {0.0, -0.3, 0.0, 0.0, 0.0, 0.2}, "compact-active",
+                {}, 2, 3, 3, 3, 3, 3, 3},
         CaseDef{"block0_only", {0.0, 0.0, 0.7, -1.2, 0.5}, "compact-active",
                 {ExpectedBlock{"block0", 3, "One"}}, 0, 5, 5, 5, 2, 5, 2},
         CaseDef{"block1_only", {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.4, 0.9, 1.1}, "compact-active",
